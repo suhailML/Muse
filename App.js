b54845c03d7f1f1c06cdcc24d27f8ba27bedcb23
Elisson Espinal <Elisson@dhcp-wifi-8021x-155-41-73-188.bu.edu>
2019-11-10 18:09:16 -0500

    Dev Branch: Refactored code to enforce modularization. TODO: Efficiently include components in App.js

--- App.js
+++ App.js
@@ -1,56 +1,13 @@
 import React from 'react';
-import { StyleSheet, Text, View } from 'react-native';
-import { createAppContainer } from 'react-navigation';
-import {createBottomTabNavigator } from 'react-navigation-tabs';
+import {StyleSheet, Text, View} from 'react-native';
+import {createAppContainer} from 'react-navigation';
+import {createBottomTabNavigator} from 'react-navigation-tabs';
 
-
-
-class HomeScreen extends React.Component {
-  render() {
-    return(
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text> This is my Home screen </Text>
-      </View>
-    );
-  }
-}
-
-class SearchScreen extends React.Component {
-  render() {
-    return(
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text> This is my Search screen </Text>
-      </View>
-    );
-  }
-}
-class MicrophoneScreen extends React.Component {
-  render() {
-    return(
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text> This is my Recording screen </Text>
-      </View>
-    );
-  }
-}
-class MessageScreen extends React.Component {
-  render() {
-    return(
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text> This is my Message screen </Text>
-      </View>
-    );
-  }
-}
-class ProfileScreen extends React.Component {
-  render() {
-    return(
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text> This is my Profile screen </Text>
-      </View>
-    );
-  }
-}
+import SearchScreen from './components/SearchScreen';
+import MicrophoneScreen from './components/MicrophoneScreen';
+import MessageScreen from './components/MessageScreen';
+import HomeScreen from './components/HomeScreen';
+import ProfileScreen from './components/ProfileScreen';
 
 const bottomTabNavigator = createBottomTabNavigator(
   {
