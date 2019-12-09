8e41fdbc521a485bd9c44f399883abda8186469b
miasz <43791471+miasz@users.noreply.github.com>
2019-12-09 16:18:19 -0500

    Dev Branch: Initial implementation oof a basica Messages screen and a Profiles screen

--- components/MessageScreen.js
+++ components/MessageScreen.js
@@ -1,12 +1,126 @@
-import React, {Component} from "react";
-import { View, Text } from "react-native";
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  TouchableOpacity, 
+  Button,
+  SafeAreaView,
+  Alert,
+  TouchableHighlight,
+  NavigatorIOS,
+} from 'react-native';
 
-export default class MessageScreen extends Component {
+export default class Profile extends Component {
+
   render() {
-    return(
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text> This is my Message screen </Text>
+    return (
+   <SafeAreaView style={styles.container}>
+    <ScrollView>
+      <View style={{flex: 1}}>
+
+
+
+
+
+		<Text style={styles.Username}>Username</Text>
+       	<Image style={styles.back} source={{uri: 'https://www.pngix.com/pngfile/big/267-2677471_back-ios-comments-ios-back-button-image-png.png'}}/>
+		<Image style={styles.write} source={{uri: 'https://s3.us-east-2.amazonaws.com/upload-icon/uploads/icons/png/20092061831530273514-128.png'}}/>
+      	<Image style={styles.avatar} source={{uri: 'https://ewans.files.wordpress.com/2008/07/grey1.jpg'}}/>
+        <Image style={styles.avatar1} source={{uri: 'https://ewans.files.wordpress.com/2008/07/grey1.jpg'}}/>
+         <Image style={styles.avatar2} source={{uri: 'https://ewans.files.wordpress.com/2008/07/grey1.jpg'}}/>
+         <View style={styles.body}>
+         	<View style={styles.bodyContent}>
+            <Text style={styles.name}>Name</Text>
+            <Text style={styles.Message}>Last Message Sent</Text>
+            <Text style={styles.name}>Name</Text>
+            <Text style={styles.Message}>Last Message Sent</Text>
+            <Text style={styles.name}>Name</Text>
+            <Text style={styles.Message}>Last Message Sent</Text>
+            </View>
+        </View>
       </View>
+      </ScrollView>
+      </SafeAreaView>
     );
   }
 }
+
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor:'#d3d3d5'
+  }, 
+  safe: {
+    flex: 1,
+  },
+  back:{
+  	width: 25,
+    height: 25,
+    marginLeft: 10,
+    position: 'absolute',
+  },
+  write:{
+  	width: 25,
+    height: 25,
+    marginLeft: 330,
+    position: 'absolute',
+  },
+  Username:{
+    fontSize:20,
+    color: "#000000",
+    textAlign: 'center',
+    marginBottom: 5,
+    fontWeight: "600",
+    position: 'absolute',
+    marginLeft:135,
+    marginRight:25,
+  },
+  avatar: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom:5,
+    position: 'absolute',
+    marginTop:25,
+    marginLeft:25
+  },
+   avatar1: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom:5,
+    position: 'absolute',
+    marginTop:150,
+    marginLeft:25
+  },
+   avatar2: {
+    width: 100,
+    height: 100,
+    borderRadius: 50,
+    marginBottom:5,
+    position: 'absolute',
+    marginTop:275,
+    marginLeft:25
+  },
+  name:{
+    fontSize:20,
+    color: "#000000",
+    marginTop:50,
+    marginLeft: 35,
+    textAlign: 'center',
+    marginBottom: 5,
+    fontWeight: "600",
+  },
+   Message:{
+    fontSize:12,
+    color: "#000000",
+    marginTop:5,
+    marginLeft: 90,
+    textAlign: 'center',
+    marginBottom: 25,
+    fontWeight: "300",
+  },
+});
--- components/ProfileScreen.js
+++ components/ProfileScreen.js
@@ -1,13 +1,103 @@
-import React, {Component} from "react";
-import { View, Text } from "react-native";
+import React, { Component } from 'react';
+import {
+  StyleSheet,
+  Text,
+  View,
+  Image,
+  ScrollView,
+  TouchableOpacity, 
+  Button,
+  SafeAreaView,
+  Alert,
+  TouchableHighlight,
+  NavigatorIOS,
+} from 'react-native';
+//  add button functionality if need be 
+export default class Profile extends Component {
 
-export default class ProfileScreen extends Component {
   render() {
-    return(
-      <View style={{flex: 1, justifyContent: 'center', alignItems: 'center'}}>
-        <Text> This is my Profile screen </Text>
+    return (
+   <SafeAreaView style={styles.container}>
+    <ScrollView>
+      <View style={{flex: 1}}>
+          <Image style = {styles.gear} source = {{uri: 'https://www.jing.fm/clipimg/full/51-515158_setting-clipart-black-png-settings-button.png'}}/>
+          <Image style={styles.avatar} source={{uri: 'https://ewans.files.wordpress.com/2008/07/grey1.jpg'}}/>
+          <View style={styles.body}>
+            <View style={styles.bodyContent}>
+            <Text style={styles.name}>Name</Text>
+            <Text style={styles.Bio}>Bio</Text>
+            <Text style={styles.Description}>nvfdjknvdfjknvdflkjnvfldkjnvlfnvjflknkdnvkdfvnfcvbcsdfsdfsghg </Text>
+            <Image style={styles.boxes} source={{uri:'https://ewans.files.wordpress.com/2008/07/grey1.jpg'}}/>
+            <Image style={styles.boxes} source={{uri:'https://ewans.files.wordpress.com/2008/07/grey1.jpg'}}/>
+            <Image style={styles.boxes} source={{uri:'https://ewans.files.wordpress.com/2008/07/grey1.jpg'}}/>
+            <Image style={styles.boxes} source={{uri:'https://ewans.files.wordpress.com/2008/07/grey1.jpg'}}/>
+            </View>
+        </View>
       </View>
+      </ScrollView>
+      </SafeAreaView>
     );
   }
 }
 
+const styles = StyleSheet.create({
+  container:{
+    flex: 1,
+    backgroundColor:'#d3d3d5'
+  }, 
+  gear: {
+    width: 25,
+    height: 25,
+    marginLeft: 325,
+    marginTop: 10,
+  },
+  safe: {
+    flex: 1,
+  },
+  avatar: {
+    width: 125,
+    height: 125,
+    borderRadius: 64,
+    marginBottom:5,
+    position: 'absolute',
+    marginTop:25,
+    marginLeft:25
+  },
+  name:{
+    fontSize:20,
+    color: "#000000",
+    marginTop:15,
+    marginLeft: 35,
+    textAlign: 'center',
+    fontWeight: "600"
+  },
+  Bio:{
+    fontSize:16,
+    color: "#000000",
+    marginTop:10,
+    marginLeft: 5,
+    textAlign: 'center',
+  },
+  Description:{
+    fontSize:12,
+    color: "#000000",
+    marginTop:10,
+    marginLeft:177,
+    marginRight: 20,
+    textAlign: 'right',
+    flexDirection: 'row',
+    flex: 1,
+  },
+  boxes:{
+    flex: 1,
+    width: 320,
+    height: 150,
+    marginTop: 50,
+    marginBottom: 15,
+    justifyContent: 'center',
+    flexDirection: 'column',
+    alignItems: 'center',
+    marginLeft: 20,
+    // try to center this more? could end up in bugs if i turn these into buttons because align items isnt working lol siq 
+  }
+});
